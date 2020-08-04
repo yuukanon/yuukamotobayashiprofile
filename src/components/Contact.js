18301b8f633994b7ae89ecec77b0defc27445ea8
Yuuka Motobayashi <yuukanon@gmail.com>
2020-08-04 19:55:30 +0200

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div>contact us form</div>
+    <div>
+      <h3>Tell me what you are looking for!</h3>
+      <form className="contact-form" method="POST" action="/contact">
+        <div className="form-field">
+          <label htmlFor="name">
+            <input type="text" name="name" placeholder="Your name" required />
+          </label>
+        </div>
+
+        <div className="form-field">
+          <label htmlFor="email">
+            <input type="email" name="email" placeholder="Your email"required />
+          </label>
+        </div>
+
+        <div className="form-field">
+          <label htmlFor="message">
+            <textarea className="project" name="message" placeholder="Tell me your project"rows="6" required />
+          </label>
+        </div>
+
+        <button type="submit" className="submit">Submit</button>
+
+        <div>
+          { window.location.hash === '#success' &&
+            <div id="success">
+              <p>Your message has been sent!</p>
+            </div>
+          }
+          { window.location.hash === '#error' &&
+            <div id="error">
+              <p>An error occured while submitting the form.</p>
+            </div>
+          }
+        </div>
+      </form>
+
+    </div>
   )
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return(
+     <header>
+        <img src="https://images.unsplash.com/photo-1489533119213-66a5cd877091?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80" alt="" />
+        <div className='name'>
+          <h3>Hello, I am Yuuka Motobayashi and</h3>
+          <h1>Project Management Professinal, Front End Web Developer</h1>
+        </div>
+      </header>
+      
+  )
+}
+
+export default Home;
