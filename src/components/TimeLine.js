1b017de615acc8c18e4ee02494521415da89254e
Yuuka Motobayashi <yuukanon@gmail.com>
2020-08-07 15:07:45 +0200

    timeline fadein https://codesandbox.io/s/beautiful-wiles-k23w5\?from-embed\=\&file\=/src/styles.css:59-378

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -1,39 +1,69 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
+function FadeInSection(props) {
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
 
 const TimeLine = () => {
+
   return (
-   <section className="timeline">
-     <div className="container left">
-        <div className="timelinecontent">
-          <h2 className="timelineh2">Title</h2>
-          <div className="date">1 MAY 2016</div>
-          <p className="timeline-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-          <a className="btn-more">More</a>
-        </div>
-      </div>
-        
-      <div className="container right">
-        <div className="timelinecontent">
-          <h2 className="timelineh2">Title</h2>
-          <div className="date">1 MAY 2016</div>
-          <p className="timeline-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-          <a className="btn-more">More</a>
-        </div>
+    <section className="timeline">
+      
+      <div className="container left">
+        <FadeInSection>
+          <div className="timelinecontent">
+              <h2 className="timelineh2">Title</h2>
+              <div className="date">1 MAY 2016</div>
+              <p className="timeline-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
+              <a className="btn-more">More</a>
+          </div>
+        </FadeInSection>    
       </div>
 
-      <div className="container left">
-        <div className="timelinecontent">
-          <h2 className="timelineh2">Title</h2>
-          <div className="date">1 MAY 2016</div>
-          <p className="timeline-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-          <a className="btn-more">More</a>
+        <div className="container right">
+          <FadeInSection>
+            <div className="timelinecontent">
+            <h2 className="timelineh2">Title</h2>
+            <div className="date">1 MAY 2016</div>
+              <p className="timeline-p">Lorem ipsum  dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
+              <a className="btn-more">More</a>
+            </div>
+            </FadeInSection>    
         </div>
-      </div>
-   </section>
-  
+
+        <div className="container left">
+          <FadeInSection>
+            <div className="timelinecontent">
+              <h2 className="timelineh2">Title</h2>
+              <div className="date">1 MAY 2016</div>
+              <p className="timeline-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
+              <a className="btn-more">More</a>
+            </div>
+            </FadeInSection> 
+          </div>
+    </section>
   )
 }
 
+
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<TimeLine />, rootElement);
 
 export default TimeLine;
--- src/components/TimeLine2.js
+++ src/components/TimeLine2.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const TimeLine2 = () => {
-  return (
-   <>
-    <div>
-      <p>Year:2020 April to Present</p>
-      <p>Year:Project Manager</p>
-      <p>Year:2020 April to Present</p>
-    </div>
-    <hr />
-      <div>
-      <p>Year:2020 April to Present</p>
-      <p>Year:Project Manager</p>
-      <p>Year:2020 April to Present</p>
-    </div>
-   </>
 
-   
+  const [open, setOpen] = useState(false);
+
+
+  return (
+   <section className="timeline">
+     <div className="container left" onClick={() => setOpen(!open)}>
+        {open && <div className="timelinecontent">
+          <h2 className="timelineh2">Title</h2>
+          <div className="date">1 MAY 2016</div>
+          <p className="timeline-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
+          <a className="btn-more">More</a>
+        </div>}
+      </div>
+        
+      <div className="container right" onClick={() => setOpen(!open)}>
+        {open && <div className="timelinecontent">
+          <h2 className="timelineh2">Title</h2>
+          <div className="date">1 MAY 2016</div>
+          <p className="timeline-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
+          <a className="btn-more">More</a>
+        </div>}
+      </div>
+
+      <div className="container left" onClick={() => setOpen(!open)}>
+        {open && <div className="timelinecontent">
+          <h2 className="timelineh2">Title</h2>
+          <div className="date">1 MAY 2016</div>
+          <p className="timeline-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
+          <a className="btn-more">More</a>
+        </div>}
+      </div>
+   </section>
+  
   )
 }
 
