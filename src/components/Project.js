e85adba334e8c1ecaf0ce4c0da0cfdf3d461748b
Yuuka Motobayashi <yuukanon@gmail.com>
2020-09-27 12:19:49 +0200

    floating box

--- src/components/Project.js
+++ src/components/Project.js
@@ -16,32 +16,46 @@ const Project = () => {
           <div className="image-container">
             <img src={pic1} />
           </div>
-          <p className="slide-box">
+          {/* <p className="slide-box">
               <h3>First Website</h3>
               <p>The first website created as a course project from scratch with HTML, CSS, and Javascript.</p>
               <button><a href="https://github.com/yuukanon/homeexpert">Check it in GitHub  <FontAwesomeIcon icon={['fab', 'github']} size="lg" /></a></button>
-          </p>
-          
+          </p> */}
+          <div className="project-text">
+            <h3>First Website</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat minus excepturi assumenda optio sint consectetur, ipsa animi?</p>
+            <button>click here</button>
+          </div>
         </div>
         <div className="each-fade">
         <div className="image-container">
             <img src={pic2} />
           </div>
-          <p className="slide-box">
+          {/* <p className="slide-box">
               <h3>Weather App</h3>
               <p>Used React API to call 5 day weather forecast based on a Searched city.</p>
               <button><a href="https://github.com/yuukanon/weatherapp">Check it in GitHub  <FontAwesomeIcon icon={['fab', 'github']} size="lg" /></a></button>
-          </p>
+          </p> */}
+          <div className="project-text">
+            <h3>First Website</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat minus excepturi assumenda optio sint consectetur, ipsa animi?</p>
+            <button>click here</button>
+          </div>
         </div>
         <div className="each-fade">
         <div className="image-container">
             <img src={pic3} />
           </div>
-          <p className="slide-box">
+          {/* <p className="slide-box">
               <h3>Hackathon</h3>
               <p>Developed a product landing page for Nokia Featured Phone less than 24 hours.</p>
               <button><a href="https://github.com/yuukanon/homeexpert">Check it in GitHub  <FontAwesomeIcon icon={['fab', 'github']} size="lg" /></a></button>
-          </p>
+          </p> */}
+          <div className="project-text">
+            <h3>First Website</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat minus excepturi assumenda</p>
+            <button>click here</button>
+          </div>
         </div>
       </Fade>
     </div>
