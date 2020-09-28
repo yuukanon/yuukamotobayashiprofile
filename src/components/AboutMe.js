1714944689186a3d2e3fd6a2c3e5735216df5f57
Yuuka Motobayashi <yuukanon@gmail.com>
2020-09-28 15:31:27 +0200

    css for tablet

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,14 +8,13 @@ const AboutMe = () => {
     <div className="aboutMe">
       <div className="profile">
         <div className="profilecontent">
-          <h3>Not Just a Front End Web Developer but also </h3>
-          <h3>Certified Project Management Professional</h3><br></br>
+          <h3>Not Just a Front End Web Developer but also Certified Project Management Professional</h3>
           <li>
               Results-oriented Front End Web Developer dedicated to creating interactive, user-friendly, and feature-rich websites. 
-          </li><br></br>
+          </li>
           <li>
              Building new websites from start to finish by gathering clients' business requirements and providing technical knowledge and expertise. 
-          </li><br></br>
+          </li>
 
           <li>
             Proven organizational and business skills with 10+ years of managing functional to enterprise level project experiences.
